e2bc9ab76cf12d9e059819430e7d5f9b035567b7
Johan <johan.bongailas.e22072@mcast.edu.mt>
2025-03-02 12:05:19 +0000

    Created student model and enabled mass assignment

---
+++ app/Models/Student.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+    // Allow mass assignment
+    protected $fillable = ["name", "email", "phone", "dob", "college_id"];
+}
