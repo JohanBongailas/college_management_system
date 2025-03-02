9b7080984e6f288058b1dde45a05479f1d09b8e0
Johan <johan.bongailas.e22072@mcast.edu.mt>
2025-03-02 16:20:49 +0000

    Added the resourceful routes for both the colleges and the students

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CollegeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('colleges', CollegeController::class);
+Route::resource('students', StudentController::class);
