e8ad38ea7cab8639bca798b8b63b40c168004474
Johan <johan.bongailas.e22072@mcast.edu.mt>
2025-03-02 11:42:04 +0000

    Mirgrated the students migration as I forgot to migrate it

--- database/migrations/2025_03_01_121748_create_students.php
+++ database/migrations/2025_03_01_121748_create_students.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable(false);
+            $table->string('name', 70)->nullable(false);
             $table->string('email')->unique()->nullable(false);
             $table->string('phone')->nullable(false);
             $table->date("dob")->nullable(false);
